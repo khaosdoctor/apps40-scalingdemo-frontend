b24f954471a58245b21c4863561775208fb6e0df
Lucas Santos <lhs.santoss@gmail.com>
2020-04-06 17:32:59 -0300

    🥅 Update error to include better objects

--- backend/src/errors/KubernetesAPIError.ts
+++ backend/src/errors/KubernetesAPIError.ts
@@ -1,7 +1,9 @@
 import { format } from 'util'
 
 export class KubernetesAPIError extends Error {
+  errorObject: any
   constructor (message: string) {
     super(format(`KUBERNETES API ERROR: ${message}`))
+    this.errorObject = message
   }
 }
--- backend/src/presentation/routes/kubernetes/deployments.ts
+++ backend/src/presentation/routes/kubernetes/deployments.ts
@@ -14,7 +14,7 @@ export default function (service: KubernetesService) {
         .json(entity)
     }),
     (err: any, _req: Request, _res: Response, next: NextFunction) => {
-      if (err instanceof KubernetesAPIError) next(boom.badGateway(err.message, { code: 'kubernetes_api_error' }))
+      if (err instanceof KubernetesAPIError) next(boom.badGateway(err.message, { code: 'kubernetes_api_error', errorObject: err.errorObject }))
       next(err)
     }
   ]
